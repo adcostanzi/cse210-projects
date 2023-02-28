036d912fa4ff0c62d6ea4f2206ad5593e9ffeb1e
adcostanzi <andres.costanzi@outlook.com>
2023-02-27 22:55:29 -0600

    completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square cuadrado = new Square("blue", 4);
+   
+
+        Rectangle rectangulo = new Rectangle("green", 4, 6);
+ 
+
+        Circle circulo = new Circle("red", 2);
+ 
+        List<Shape> ShapeList = new List<Shape>();
+
+        ShapeList.Add(cuadrado);
+        ShapeList.Add(rectangulo);
+        ShapeList.Add(circulo);
+
+        foreach (Shape forma in ShapeList)
+        {
+            Console.WriteLine($"The shape is {forma.GetColor()} and the area is {forma.GetArea()}.");
+        }
     }
 }
